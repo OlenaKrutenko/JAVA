ebc06752adb08b66f3df45324bd3b0d9518c4174
OlenaKrutenko <krutenkoolena@gmail.com>
2022-06-19 15:36:27 +0300

    add new file

---
+++ loop1.java
@@ -0,0 +1,17 @@
+public class Main {
+        public static void main (String[] args){
+            int money = 10;
+            if(money==5)
+            {
+                System.out.println("You can buy a candy.");
+            }
+            else if(money==10)
+            {
+                System.out.println("You can buy 2 candies or one bar of chocolate.");
+            }
+            else if(money==15)
+            {
+                System.out.println("You can buy a cake, or one candy and one bar of chocolate, or three candies.");
+            }
+        }
+}

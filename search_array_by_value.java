d684fa1e5a92885de9e99f607f6e9abb4922b80c
OlenaKrutenko <krutenkoolena@gmail.com>
2022-06-21 22:35:39 +0300

    add file

---
+++ search_array_by_value.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+import java.util.Scanner;
+public class Main {
+     public static void main(String[] args){
+          int[] grades = {1, 2, 3, 72, 5};
+
+         for(int i=0; i< grades.length; i++){
+               if(grades[i] == 72) {
+      System.out.println("Found!");
+
+	       }
+	 }
+     }
+}

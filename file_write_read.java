117764a6feefa9692a2d98a1c61bc458f518edd9
OlenaKrutenko <krutenkoolena@gmail.com>
2022-06-22 10:41:41 +0300

    add file

---
+++ file_write_read.java
@@ -0,0 +1,25 @@
+[200~import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+public class Main { 
+     	public static void main(String[] args) throws IOException{
+             FileWriter myFilew = new FileWriter("my_information.txt");			               myFilew.write("My name is Kevin.\n");
+		myFilew.write("I am an English teacher.\n");
+		    myFilew.close();
+
+	     FileReader myFiler = new FileReader ("my_information.txt");
+              System.out.println("The first reading method:");
+            Scanner scan = new Scanner (myFiler);				            int i = 1;
+										           while (scan.hasNextLine()){
+								                System.out.println(i + ": " + scan.nextLine());						            i++;																	            }
+	 scan.close();							           myFiler.close();
+									          System.out.println("\n The second reading method:");
+									         myFiler = new FileReader ("my_information.txt");
+										        char[] array = new char[100];
+		
+			 								myFiler.read(array);
+									              System.out.println(array);								       
+												 myFiler.close();
+										     }
+}

7f1fa991febfef67331a2807ac27c7b54af9c44c
OlenaKrutenko <krutenkoolena@gmail.com>
2022-06-24 15:50:55 +0300

    add file

---
+++ method_equals.java
@@ -0,0 +1,11 @@
+
+import java.util.Arrays;
+public class Main { 
+	     public static void main(String[] args) {
+		            int [] grade1 = {1, 2, 7, 8, 25};
+			           int [] grade2 = {1, 2, 7, 8, 25};
+				          if (Arrays.equals(grade1, grade2)) {
+						           System.out.println("Equals Finally");
+							              }
+					       }
+}

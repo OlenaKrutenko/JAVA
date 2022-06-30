dc78f07f7d31dc92bdef33ccea46b66e0dedb779
OlenaKrutenko <krutenkoolena@gmail.com>
2022-07-01 00:03:16 +0300

    add file

---
+++ booleans.java
@@ -0,0 +1,23 @@
+//Boolean Values
+
+boolean isJavaFun = true;
+boolean isFishTasty = false;
+System.out.println(isJavaFun);     // Outputs true
+System.out.println(isFishTasty);   // Outputs false
+
+//Boolean Expression
+ int x = 10;
+int y = 9;
+System.out.println(x > y); // returns true, because 10 is higher than 9
+
+// equal to (==)
+int x = 10;
+System.out.println(x == 10); // returns true, because the value of x is equal to 10
+
+// example
+
+boolean  isJavaFun = true;
+boolean  isFishTasty = false;
+System.out.println(isJavaFun);
+System.out.println(isFishTasty);
+

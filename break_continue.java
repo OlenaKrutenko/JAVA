75bc2c7e891af560f834c3f9e1c10fd7b6a8e78f
OlenaKrutenko <92334489+OlenaKrutenko@users.noreply.github.com>
2022-07-02 18:13:17 +0300

    Update file

--- break_continue.java
+++ break_continue.java
@@ -1,11 +1,18 @@
+
+//The "break" statement can  jump out of a loop.
+//Stops the loop when i is equal to 4
 for (int i = 0; i < 10; i++) {
   if (i == 4) {
     break;
   }
   System.out.println(i);
+}
+
+//The continue statement breaks one iteration (in the loop), if a specified condition occurs, and continues with the next iteration in the loop.
+//Skips the value of 4:
 for (int i = 0; i < 10; i++) {
   if (i == 4) {
     continue;
   }
   System.out.println(i);
-}}
+}

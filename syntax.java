22f11aab730300dde0660045e496e8d476f15428
OlenaKrutenko <92334489+OlenaKrutenko@users.noreply.github.com>
2022-07-03 15:44:32 +0300

    Update syntax.java

--- syntax.java
+++ syntax.java
@@ -1,3 +1,4 @@
+//Java program has a class name which must match the filename, and that every program must contain the main() method.
 public class Main {//Every line of code that runs in Java must be inside" a claas". The name of the java file must match the class name
 
   public static void main(String[] args) {

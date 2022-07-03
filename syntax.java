38e0d1bfb4ffc5d862bebb1b674c20800fb89a46
OlenaKrutenko <krutenkoolena@gmail.com>
2022-07-03 15:41:57 +0300

    add new file

---
+++ syntax.java
@@ -0,0 +1,7 @@
+public class Main {//Every line of code that runs in Java must be inside" a claas". The name of the java file must match the class name
+
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+  }
+}
+

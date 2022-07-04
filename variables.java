2e90480b201aabf4243c5860b0205e384a183341
OlenaKrutenko <krutenkoolena@gmail.com>
2022-07-04 15:36:52 +0300

    add file

---
+++ variables.java
@@ -0,0 +1,21 @@
+int myNum = 5;
+float myFloatNum = 5.99f;
+char myLetter = 'D';
+boolean myBool = true;
+String myText = "Hello";
+
+int myNum;
+myNum = 15;
+System.out.println(myNum);
+
+
+
+int myNum = 15;
+myNum = 20;  // myNum is now 20
+System.out.println(myNum);
+
+
+//"final" or "constant"
+final int myNum = 15;
+myNum = 20;  // will generate an error: cannot assign a value to a final variable
+

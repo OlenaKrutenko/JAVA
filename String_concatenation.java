ba7398b06ec7a11acfee6c0a17a33d4567445c28
OlenaKrutenko <krutenkoolena@gmail.com>
2022-07-08 23:17:45 +0300

    add file

---
+++ String_concatenation.java
@@ -0,0 +1,10 @@
+String firstName = "John ";
+String lastName = "Doe";
+System.out.println(firstName.concat(lastName));
+
+//
+String firstName = "John";
+String lastName = "Doe";
+System.out.println(firstName + " " + lastName);
+
+

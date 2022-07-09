04b57b7fada28ac058b05068a96d349aca4d5cac
OlenaKrutenko <krutenkoolena@gmail.com>
2022-07-09 18:15:39 +0300

    add file

---
+++ adding_string_number.java
@@ -0,0 +1,19 @@
+//If  add two numbers, the result will be a number:
+ int x = 10;
+int y = 20;
+int z = x + y;  // z will be 30 (an integer/number)
+
+
+//If you  two strings, the result will be a string concatenation:
+ String x = "10";
+String y = "20";
+String z = x + y;  // z will be 1020 (a String)
+
+
+//If  add a number and a string, the result will be a string concatenation:
+String x = "10";
+int y = 20;
+String z = x + y;  // z will be 1020 (a String)
+
+
+
